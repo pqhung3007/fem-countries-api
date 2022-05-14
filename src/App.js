bedf55e0bd21324744224c18e4c02e66e7e43028
Windows 10 <hungpham307@gmail.com>
2022-05-14 17:19:40 +0700

    fetch all countries and style main page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Countries from 'components/Countries'
+import Countries from './components/Countries'
 
 function App() {
   return (
--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,8 +1,48 @@
-import React, { Component } from "react";
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+const url = "https://restcountries.com/v3.1/all";
 
 const Countries = () => {
-  return <div>Countries</div>;
+  const [countries, setCountries] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchCountries = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setCountries(data);
+  };
+
+  useEffect(() => {
+    fetchCountries();
+  }, []);
+
+  return (
+    <div className="countries">
+      {countries.map((country, index) => {
+        const { name, flags, population, region, capital } = country;
+
+        return (
+          <article key={index}>
+            <div className="flag">
+              <img src={flags.png} alt={name} />
+            </div>
+            <div className="details">
+              <h3>{name.common}</h3>
+              <h4>
+                Population: <span>{population.toLocaleString()}</span>
+              </h4>
+              <h4>
+                Region: <span> {region}</span>
+              </h4>
+              <h4>
+                Capital: <span>{capital}</span>
+              </h4>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Countries;
