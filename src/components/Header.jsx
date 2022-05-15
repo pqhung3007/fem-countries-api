ae770cdfff757e9a8b6d9bd46d30376a0a8ef419
Windows 10 <hungpham307@gmail.com>
2022-05-15 16:37:47 +0700

    implement dark/night mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FaMoon, FaSun } from "react-icons/fa";
 
 const Header = () => {
+  const [theme, setTheme] = useState("dark-theme");
+
+  const changeTheme = () => {
+    if (theme === "dark-theme") {
+      setTheme("light-theme");
+    } else {
+      setTheme("dark-theme");
+    }
+  };
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+  }, [theme]);
+
   return (
     <header className="header">
       <div>
@@ -9,7 +23,7 @@ const Header = () => {
       </div>
 
       <div>
-        <button className="btn-moon">
+        <button className="btn-moon" onClick={changeTheme}>
           <FaMoon />
           <FaSun />
           Change Theme
